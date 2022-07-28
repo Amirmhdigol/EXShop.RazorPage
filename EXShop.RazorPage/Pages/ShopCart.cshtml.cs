af420a49b70a4a9d0118ed7eab56cbd560e2be62
Amirmhdigol <amirmhdigol82@gmail.com>
2022-07-29 03:36:23 +0430

    ShopCartAdded

--- EXShop.RazorPage/Pages/ShopCart.cshtml.cs
+++ EXShop.RazorPage/Pages/ShopCart.cshtml.cs
@@ -1,12 +1,60 @@
+using EXShop.RazorPage.Infrastructure;
+using EXShop.RazorPage.Models.Orders;
+using EXShop.RazorPage.Services.Orders;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace EXShop.RazorPage.Pages
+namespace EXShop.RazorPage.Pages;
+public class ShopCartModel : BaseRazorPage
 {
-    public class ShopCartModel : PageModel
+    private readonly IOrderService _orderService;
+    public ShopCartModel(IOrderService orderService)
     {
-        public void OnGet()
+        _orderService = orderService;
+    }
+
+    public OrderDTO? OrderDto { get; set; }
+    public async Task OnGet()
+    {
+        if (User.Identity.IsAuthenticated)
         {
+            OrderDto = await _orderService.GetCurrentOrder();
+        }
+        else
+        {
+            
+        }
+    }
+    public async Task<IActionResult> OnPostDeleteItem(long id)
+    {
+        if (User.Identity.IsAuthenticated)
+        {
+            return await AjaxTryCatch(() => _orderService.RemoveOrderItem(new RemoveOrdertemCommand
+            {
+                ItemId = id
+            }));
+            return RedirectToPage("ShopCart");
+        }
+        else
+        {
+            return Page();
+        }
+    }
+
+    public async Task<IActionResult> OnPostAddItem(long inventoryId, int count)
+    {
+        if (User.Identity.IsAuthenticated)
+        {
+            return await AjaxTryCatch(() => _orderService.AddOrderItem(new AddOrderItemCommand
+            {
+                UserId = User.GetUserId(),
+                InventoryId = inventoryId,
+                Count = count
+            }));
+        }
+        else
+        {
+            return Page();
         }
     }
 }
--- EXShop.RazorPage/Services/Orders/IOrderService.cs
+++ EXShop.RazorPage/Services/Orders/IOrderService.cs
@@ -4,11 +4,11 @@ using EXShop.RazorPage.Models.Orders;
 namespace EXShop.RazorPage.Services.Orders;
 public interface IOrderService
 {
-    Task<ApiResult?> AddOrderItem(AddOrderItemCommand command);
-    Task<ApiResult?> OrderCheckout(ChechOutOrderCommand command);
-    Task<ApiResult?> IncreaseItemCount(ChangeOrderItemCountCommand command);
-    Task<ApiResult?> DecreaseItemCount(ChangeOrderItemCountCommand command);
-    Task<ApiResult?> RemoveOrderItem(RemoveOrdertemCommand command);
+    Task<ApiResult> AddOrderItem(AddOrderItemCommand command);
+    Task<ApiResult> OrderCheckout(ChechOutOrderCommand command);
+    Task<ApiResult> IncreaseItemCount(ChangeOrderItemCountCommand command);
+    Task<ApiResult> DecreaseItemCount(ChangeOrderItemCountCommand command);
+    Task<ApiResult> RemoveOrderItem(RemoveOrdertemCommand command);
 
     Task<OrderDTO?> GetOrderById(long orderId);
     Task<OrderDTO?> GetCurrentOrder();
